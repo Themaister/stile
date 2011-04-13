ef70e6007322021776b83fda4fa787490f13d5bb
Themaister <maister@archlinux.us>
2011-04-13 22:51:03 +0200

    Option stuff, will be used at some point!

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <string>
 #include <stdint.h>
+#include <getopt.h>
 
 
 namespace args
@@ -17,7 +18,8 @@ namespace args
       std::string tile;
       std::string tilemap;
       std::string input;
-   } path;
+   };
+   static path_settings path;
 
    struct color_settings
    {
@@ -28,7 +30,11 @@ namespace args
       // Translates the transparent color in the palette to a certain color 
       // (e.g. have green transparent, but is translated to black when creating tiles ...)
       uint32_t translate;
-   } color;
+   };
+   static color_settings color;
+
+   static unsigned size;
+   static bool verbose;
 
    static void init_args()
    {
@@ -45,6 +51,22 @@ namespace args
 
    static void print_help()
    {
+      std::cout << "=====================================" << std::endl;
+      std::cout << " stile - Simple SNES tile exporter" << std::endl;
+      std::cout << "=====================================" << std::endl;
+      std::cout << std::endl;
+      std::cout << "\tUsage: stile [OPTIONS...]" << std::endl;
+      std::cout << std::endl;
+      std::cout << "  -f/--file: Input file." << std::endl;
+      std::cout << "  -o/--output: Output base path (extensions will be appended)." << std::endl;
+      std::cout << "  -c/--colors: Number of colors to use (default: 16)." << std::endl;
+      std::cout << "  -S/--sprite: Sprite mode. Do not export tile map." << std::endl;
+      std::cout << "  -s/--size: Override tile/sprite size (default: 8)." << std::endl;
+      std::cout << "  -t/--transparent: Pick transparent color in XBGR hex format." << std::endl;
+      std::cout << "  -T/--translate: Translates transparent color." << std::endl;
+      std::cout << "  -h/--help: This help." << std::endl;
+      std::cout << "  -v/--verbose: Verbose logging." << std::endl;
+      std::cout << std::endl;
    }
 
    static void parse(int argc, char **argv)
@@ -55,6 +77,77 @@ namespace args
       {
          print_help();
          throw std::runtime_error("Not enough arguments given.");
+      }
+
+      int val = 0;
+      struct option opts[] = {
+         { "help", 0, NULL, 'h' },
+         { "file", 1, NULL, 'f' },
+         { "output", 1, NULL, 'o' },
+         { "colors", 1, NULL, 'c' },
+         { "sprite", 0, NULL, 'S' },
+         { "size", 1, NULL, 's' },
+         { "transparent", 1, NULL, 't' },
+         { "translate", 1, NULL, 'T' },
+         { "verbose", 0, NULL, 'v' }
+      };
+
+      int option_index = 0;
+      char optstring[] = "hf:o:c:Ss:vt:T:";
+      for (;;)
+      {
+         val = 0;
+         int c = getopt_long(argc, argv, optstring, opts, &option_index);
+
+         if (c == -1)
+            break;
+
+         switch (c)
+         {
+            case 'h':
+               print_help();
+               exit(0);
+
+            case 'f':
+               args::path.input = optarg;
+               break;
+
+            case 'o':
+               args::path.base = optarg;
+               break;
+
+            case 'c':
+               args::color.num = strtoul(optarg, NULL, 0);
+               break;
+
+            case 's':
+               args::size = strtoul(optarg, NULL, 0);
+               break;
+
+            case 't':
+               args::color.transparent = strtoul(optarg, NULL, 16);
+               break;
+
+            case 'T':
+               args::color.translate = strtoul(optarg, NULL, 16);
+               break;
+
+            case 'v':
+               args::verbose = true;
+               break;
+         }
+
+         if (optind != argc)
+         {
+            print_help();
+            throw std::runtime_error("Invalid arguments.");
+         }
+
+         if (args::path.input.empty())
+         {
+            print_help();
+            throw std::runtime_error("Needs an input file!");
+         }
       }
 
       finish_args();
@@ -98,5 +191,5 @@ int main(int argc, char **argv)
       stile::Image img(argv[1]);
       output::dump_files(img);
    }
-   catch (std::exception& e) { fprintf(stderr, "%s\n", e.what()); }
+   catch (std::exception& e) { std::cerr << "ERROR: " << e.what() << std::endl; return 1; }
 }
